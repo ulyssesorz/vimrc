50574ad45f8c27ce460c86832777642ebf4d93b5
zhihaopan <zhihaopan@tencent.com>
2023-05-19 15:20:34 +0800

    up

--- neovim/lua/plugins/fzf.lua
+++ neovim/lua/plugins/fzf.lua
@@ -4,8 +4,8 @@ function M.config()
     vim.g.fzf_command_prefix = 'Fzf'
     vim.g.fzf_layout = {
         window = {
-            width = 0.9,
-            height = 0.6,
+            width = 0.8,
+            height = 0.85,
             border = "bold"
         }
     }
