2ecf10c69c09b21d5d6cb6e06e4f094ea4a499d0
zhihaopan <zhihaopan@tencent.com>
2023-09-11 14:33:17 +0800

    up

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,4 @@
-local function run_config(config_module)
+local function Config(config_module)
     local function is_directory_empty(path)
         local cmd = 'ls -A "' .. path .. '" |grep -v packer'
         local handle = io.popen(cmd)
@@ -16,19 +16,16 @@ local function run_config(config_module)
     require(config_module).config()
 end
 
-local function local_pack()
-end
-
 require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use {
         'tomasiser/vim-code-dark',
-        config = run_config('plugins.vim-code-dark')
+        config = Config('plugins.vim-code-dark')
     }
     use {
         'neoclide/coc.nvim',
         branch = 'release',
-        config = run_config('plugins.coc')
+        config = Config('plugins.coc')
     }
     use {
         'vim-airline/vim-airline',
@@ -38,7 +35,7 @@ require('packer').startup(function(use)
 
     use {
         'scrooloose/nerdcommenter',
-        config = run_config('plugins.nerdcommenter')
+        config = Config('plugins.nerdcommenter')
     }
     use 'honza/vim-snippets'
     use {
@@ -47,14 +44,14 @@ require('packer').startup(function(use)
         requires = {
             {
                 'junegunn/fzf.vim',
-                config = run_config('plugins.fzf')
+                config = Config('plugins.fzf')
             },
             {
                 'junegunn/fzf',
                 run = 'cd ~/.fzf && ./install --all && go build . && mv fzf bin',
             }
         },
-        config = run_config('plugins.cocfzf')
+        config = Config('plugins.cocfzf')
     }
     use {
         'fatih/vim-go',
@@ -62,35 +59,35 @@ require('packer').startup(function(use)
     }
     use {
         'ntpeters/vim-better-whitespace',
-        config = run_config('plugins.vim-better-whitespace')
+        config = Config('plugins.vim-better-whitespace')
     }
     use {
         'skywind3000/asynctasks.vim',
         requires = {
             'skywind3000/asyncrun.vim',
         },
-        config = run_config('plugins.asynctasks')
+        config = Config('plugins.asynctasks')
     }
     use {
         'voldikss/vim-floaterm',
-        config = run_config('plugins.floaterm')
+        config = Config('plugins.floaterm')
     }
     use {
         'bfrg/vim-cpp-modern',
         ft = { 'c', 'cpp' },
-        config = run_config('plugins.vim-cpp-modern')
+        config = Config('plugins.vim-cpp-modern')
     }
     use {
         'junegunn/rainbow_parentheses.vim',
-        config = run_config('plugins.rainbow_parentheses')
+        config = Config('plugins.rainbow_parentheses')
     }
     use {
         'vim-scripts/DoxygenToolkit.vim',
-        config = run_config('plugins.DoxygenToolkit')
+        config = Config('plugins.DoxygenToolkit')
     }
     use {
         'madox2/vim-ai',
-        config = run_config('plugins.vim-ai')
+        config = Config('plugins.vim-ai')
     }
     use {
         'Civitasv/cmake-tools.nvim',
@@ -101,19 +98,14 @@ require('packer').startup(function(use)
                 requires = { 'nvim-tree/nvim-web-devicons' },
             }
         },
-        config = run_config('plugins.cmake-tools')
+        config = Config('plugins.cmake-tools')
     }
     use {
-        'nvim-telescope/telescope-ui-select.nvim',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.3',
         requires = {
-            'nvim-telescope/telescope.nvim',
-            tag = '0.1.1',
-            requires = {
-                { 'nvim-lua/plenary.nvim' },
-            },
-            config = run_config('plugins.telescope')
+            { 'nvim-lua/plenary.nvim' },
         },
-        config = run_config('plugins.telescope-ui-select')
+        config = Config('plugins.telescope')
     }
-    local_pack()
 end)
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,23 @@
 local M = {}
 
 function M.config()
+    require('telescope').setup {
+        defaults = {
+            layout_strategy = 'vertical',
+        },
+    }
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<space>b', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
     vim.keymap.set('n', '<space>f', builtin.find_files, {})
     vim.keymap.set('n', '<space>a',
-        function() builtin.live_grep({ default_text = vim.fn.expand("<cword>"),
-                additional_args = { '-w', '--sort', 'path' } }) end)
+        function()
+            builtin.live_grep({
+                default_text = vim.fn.expand("<cword>"),
+                additional_args = { '-w', '--sort', 'path' }
+            })
+        end)
 end
 
 return M
